4a89f13406cd0dd9c56e331450f94e88e6e71744
Laurence Gonsalves <laurence@xenomachina.com>
2014-10-14 17:48:00 -0700

    Add nonogram.hs

---
+++ nonogram.hs
@@ -0,0 +1,8 @@
+-- A Nonogram solver
+-- by Laurence Gonsalves
+--
+-- This isn't meant to be a perfect solver. It's an attempt at implementing
+-- essentially the same process I use when solving nonograms "by hand".
+--
+-- Also, I'm writing this to learn Haskell, so my code might be
+-- weird/non-idiomatic.
